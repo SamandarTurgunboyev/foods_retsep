118da78ee54421248ff70a089bf981b35a0da47c
Samandar Turg'unboev <123394292+SamandarTurgunboyev@users.noreply.github.com>
2023-07-27 11:57:23 +0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,8 @@
 
 import Navbar from '../component/navbar/Navbar'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 import { store } from '../GlobalRedux/store'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
